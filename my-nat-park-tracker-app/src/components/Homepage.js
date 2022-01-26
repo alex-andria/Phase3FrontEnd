65421272db2dda5223c8758395508cebe548195e
Sarah Nosal <nosalsm@gmail.com>
2022-01-26 12:38:06 -0600

    updated colors, back end connected

--- my-nat-park-tracker-app/src/components/Homepage.js
+++ my-nat-park-tracker-app/src/components/Homepage.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import ListPage from './ListsPage';
-import { useEffect } from 'react';
+import ParkDetailCard from './ParkDetailCard';
+import BrowsePage from './BrowsePage';
+import { useEffect, useState } from 'react';
 // 
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 //import ParkDetails from './components/parkdetails'
 
 function HomePage(){
-    useEffect(() => {
-        // let current_lat = 37.625789;
-        // let current_long = 95.0547899;
-        // let current_zoom = 16;
-        // let center_lat = current_lat;
-        // let center_long = current_long;
-        // let center_zoom = current_zoom;
+    const [allParksData, setParksData] = useState([])
+    useEffect(()=> {
+        fetch('http://localhost:9292/parks')
+        .then(r => r.json())
+        .then(setParksData)
+    }, [])
+    // useEffect(() => {
+    //     // let current_lat = 37.625789;
+    //     // let current_long = 95.0547899;
+    //     // let current_zoom = 16;
+    //     // let center_lat = current_lat;
+    //     // let center_long = current_long;
+    //     // let center_zoom = current_zoom;
 
-        // The <div id="map"> must be added to the dom before calling L.map('map')
-          let map = L.map('map').setView([39.960,-75.210],14);
+    //     // The <div id="map"> must be added to the dom before calling L.map('map')
+    //       let map = L.map('map').setView([39.960,-75.210],14);
 
-        //alternative to api mapbox
-          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          }).addTo(map);
+    //     //alternative to api mapbox
+    //       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //       }).addTo(map);
 
         //   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -33,41 +41,41 @@ function HomePage(){
         //     accessToken: 'pk.eyJ1IjoiZGFudG9uaW9hIiwiYSI6ImNreXZwN3ZseTAwZ20ybnFwYjAyaGIxY3AifQ.qHMC48i5Mxq5Hakuzhii2w'
         // }).addTo(map);
 
-        var NPMap = {
-            div: 'map',
-            overlays: [{
-            filter: function (feature) {
-                return feature.properties.park === 'Yellowstone';
-            },
-            popup: {
-                title: '{{name}}'
-            },
-            styles: {
-                point: {
-                'marker-symbol': 'star'
-                }
-            },
-            type: 'geojson',
-            url: 'https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/gateway-points-of-interest.geojson'
-            }, {
-            popup: {
-                description: 'The alpha code is {{Code}}',
-                title: '{{Name}}'
-            },
-            styles: {
-                point: {
-                'marker-color': '#609321',
-                'marker-symbol': 'park'
-                }
-            },
-            type: 'geojson',
-            url: 'https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/national-parks.geojson'
-            }]
-        };
+        // var NPMap = {
+        //     div: 'map',
+        //     overlays: [{
+        //     filter: function (feature) {
+        //         return feature.properties.park === 'Yellowstone';
+        //     },
+        //     popup: {
+        //         title: '{{name}}'
+        //     },
+        //     styles: {
+        //         point: {
+        //         'marker-symbol': 'star'
+        //         }
+        //     },
+        //     type: 'geojson',
+        //     url: 'https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/gateway-points-of-interest.geojson'
+        //     }, {
+        //     popup: {
+        //         description: 'The alpha code is {{Code}}',
+        //         title: '{{Name}}'
+        //     },
+        //     styles: {
+        //         point: {
+        //         'marker-color': '#609321',
+        //         'marker-symbol': 'park'
+        //         }
+        //     },
+        //     type: 'geojson',
+        //     url: 'https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/national-parks.geojson'
+        //     }]
+        // };
 
         // L.geoJSON(NPMap).addTo(map);
 
-});
+// });
 
 
 
@@ -82,6 +90,11 @@ function HomePage(){
             <Route exact path='/'>
                 <h2 id='heading'>Featured National Park</h2>
                     <div id="map"></div>
+            </Route>
+            <ParkDetailCard allParksData={allParksData}/>
+
+            <Route exact path='/browsepage'>
+                <BrowsePage allParksData={allParksData} />
             </Route>
         </div>
     );
